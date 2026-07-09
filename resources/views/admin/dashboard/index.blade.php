8f10e1bf37eaa79b3d4caaf73482731b8c9dc38e
apachertr180navneeet <navneet@brenin.co>
2026-07-09 11:55:28 +0530

    feat: add animated dashboard view and custom CSS styling for enhanced UI interactivity

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.app')
 @section('content')
-<div class="container-fluid flex-grow-1 container-p-y">
+<div class="container-fluid flex-grow-1 container-p-y dashboard-animated">
     <div class="row">
         <div class="col-lg-8 mb-4 order-0">
             <div class="card premium-dashboard-card">
