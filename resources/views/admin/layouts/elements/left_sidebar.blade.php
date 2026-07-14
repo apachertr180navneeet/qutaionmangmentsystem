718bb0bd0853d0b5fd5df6cd5ba7a508b47c456a
apachertr180navneeet <navneet@brenin.co>
2026-07-14 12:22:13 +0530

    feat: create admin sidebar layout with navigation menu items

--- resources/views/admin/layouts/elements/left_sidebar.blade.php
+++ resources/views/admin/layouts/elements/left_sidebar.blade.php
@@ -20,61 +20,25 @@
 			</a>
 		</li>
 
-		<li class="menu-item {{ request()->is('admin/quotations*') ? 'active open' : ''}}">
-			<a href="javascript:void(0);" class="menu-link menu-toggle">
+		<li class="menu-item {{ request()->is('admin/quotations*') ? 'active' : ''}}">
+			<a href="{{route('admin.quotations.index')}}" class="menu-link">
 				<i class="menu-icon tf-icons bx bx-file"></i>
 				<div data-i18n="Quotations">Quotations</div>
 			</a>
-			<ul class="menu-sub">
-				<li class="menu-item {{ request()->is('admin/quotations') ? 'active' : ''}}">
-					<a href="{{route('admin.quotations.index')}}" class="menu-link">
-						<div data-i18n="All Quotations">All Quotations</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/quotations/create') ? 'active' : ''}}">
-					<a href="{{route('admin.quotations.create')}}" class="menu-link">
-						<div data-i18n="Create Quotation">Create Quotation</div>
-					</a>
-				</li>
-			</ul>
 		</li>
 
-		<li class="menu-item {{ request()->is('admin/customers*') ? 'active open' : ''}}">
-			<a href="javascript:void(0);" class="menu-link menu-toggle">
+		<li class="menu-item {{ request()->is('admin/customers*') ? 'active' : ''}}">
+			<a href="{{route('admin.customers.index')}}" class="menu-link">
 				<i class="menu-icon tf-icons bx bx-group"></i>
 				<div data-i18n="Customers">Customers</div>
 			</a>
-			<ul class="menu-sub">
-				<li class="menu-item {{ request()->is('admin/customers') ? 'active' : ''}}">
-					<a href="{{route('admin.customers.index')}}" class="menu-link">
-						<div data-i18n="All Customers">All Customers</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/customers/create') ? 'active' : ''}}">
-					<a href="{{route('admin.customers.create')}}" class="menu-link">
-						<div data-i18n="Add Customer">Add Customer</div>
-					</a>
-				</li>
-			</ul>
 		</li>
 
-		<li class="menu-item {{ request()->is('admin/items*') ? 'active open' : ''}}">
-			<a href="javascript:void(0);" class="menu-link menu-toggle">
+		<li class="menu-item {{ request()->is('admin/items*') ? 'active' : ''}}">
+			<a href="{{route('admin.items.index')}}" class="menu-link">
 				<i class="menu-icon tf-icons bx bx-box"></i>
 				<div data-i18n="Items">Items</div>
 			</a>
-			<ul class="menu-sub">
-				<li class="menu-item {{ request()->is('admin/items') ? 'active' : ''}}">
-					<a href="{{route('admin.items.index')}}" class="menu-link">
-						<div data-i18n="All Items">All Items</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/items/create') ? 'active' : ''}}">
-					<a href="{{route('admin.items.create')}}" class="menu-link">
-						<div data-i18n="Add Item">Add Item</div>
-					</a>
-				</li>
-			</ul>
 		</li>
 
 		<li class="menu-item {{ request()->is('admin/follow-ups*') ? 'active' : ''}}">
@@ -84,43 +48,11 @@
 			</a>
 		</li>
 
-		<li class="menu-item {{ request()->is('admin/reports*') ? 'active open' : ''}}">
-			<a href="javascript:void(0);" class="menu-link menu-toggle">
+		<li class="menu-item {{ request()->is('admin/reports*') ? 'active' : ''}}">
+			<a href="{{route('admin.reports.index')}}" class="menu-link">
 				<i class="menu-icon tf-icons bx bx-bar-chart-alt-2"></i>
 				<div data-i18n="Reports">Reports</div>
 			</a>
-			<ul class="menu-sub">
-				<li class="menu-item {{ request()->is('admin/reports') ? 'active' : ''}}">
-					<a href="{{route('admin.reports.index')}}" class="menu-link">
-						<div data-i18n="All Reports">All Reports</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/reports/customer-wise') ? 'active' : ''}}">
-					<a href="{{route('admin.reports.customer_wise')}}" class="menu-link">
-						<div data-i18n="Customer Wise">Customer Wise</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/reports/date-wise') ? 'active' : ''}}">
-					<a href="{{route('admin.reports.date_wise')}}" class="menu-link">
-						<div data-i18n="Date Wise">Date Wise</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/reports/status-wise') ? 'active' : ''}}">
-					<a href="{{route('admin.reports.status_wise')}}" class="menu-link">
-						<div data-i18n="Status Wise">Status Wise</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/reports/monthly') ? 'active' : ''}}">
-					<a href="{{route('admin.reports.monthly')}}" class="menu-link">
-						<div data-i18n="Monthly">Monthly</div>
-					</a>
-				</li>
-				<li class="menu-item {{ request()->is('admin/reports/item-wise') ? 'active' : ''}}">
-					<a href="{{route('admin.reports.item_wise')}}" class="menu-link">
-						<div data-i18n="Item Wise">Item Wise</div>
-					</a>
-				</li>
-			</ul>
 		</li>
 
 		<li class="menu-item {{ request()->is('admin/settings*') ? 'active' : ''}}">
